b5922263b61bd403528f961aa8d9c94cc6acda3a
Maryam <maryamjalzh@gmail.com>
2025-10-22 17:06:09 +0300

    ٤

---
+++ Learning/Learning/AllActivity.swift
@@ -0,0 +1,18 @@
+//
+//  AllActivity.swift
+//  Learning
+//
+//  Created by Maryam Jalal Alzahrani on 30/04/1447 AH.
+//
+
+import SwiftUI
+
+struct AllActivity: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AllActivity()
+}
---
+++ Learning/Learning/LearningGoal.swift
@@ -0,0 +1,18 @@
+//
+//  LearningGoal.swift
+//  Learning
+//
+//  Created by Maryam Jalal Alzahrani on 30/04/1447 AH.
+//
+
+import SwiftUI
+
+struct LearningGoal: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LearningGoal()
+}
